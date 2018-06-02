b752c117c354902758bd9f9ffd149b8c2c6a08b6
David Uhm <david.uhm@lge.com>
2018-06-02 04:01:46 +0200

    Write launch file in a pythonic way

--- 80-deep-learning/object_classifier/object_classifier/launch/image_recognition_launch.py
+++ 80-deep-learning/object_classifier/object_classifier/launch/image_recognition_launch.py
@@ -11,49 +11,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from launch.exit_handler import default_exit_handler, restart_exit_handler
 from ros2run.api import get_executable_path
 
 
+def get_cmd(package, executable, required, *args):
+    cmd = [get_executable_path(package_name=package, executable_name=executable)]
+    for arg in args:
+        cmd.append(arg)
+
+    return cmd
+
+
 def launch(launch_descriptor, argv):
     ld = launch_descriptor
-   
-    arr = [("pi_camera", "camera_node_sequence", True),
-            ("dagu_car", "wheels_driver_node", True),
-            ("joy", "joy_node", True)]
 
-    for package, executable, required in arr:
+    arg_map = {
+        'camera_topic': '/image/compressed',
+        'object_topic': '/object_classifier/output',
+        'verbose': 'True',
+        'min_score_threshold': '0.1',
+    }
+
+    for arg in argv:
+        key, value = arg.split(':=')
+        if key in arg_map:
+            arg_map[key] = value
+
+    nodes = [
+        ("pi_camera", "camera_node_sequence", True),
+        ("object_classifier", "object_classification_node", True, "--camera_topic", arg_map["camera_topic"], "--object_topic", arg_map["object_topic"], "--verbose", arg_map["verbose"], "--min_score_threshold", arg_map["min_score_threshold"]), 
+    ]
+
+    for node in nodes: 
+        package, executable, required = node[:3]
+        cmd = get_cmd(*node)
         ld.add_process(
-            cmd=[get_executable_path(package_name=package, executable_name=executable)],
+            cmd=cmd,
             name=executable,
-            # die if required, restart otherwise
             exit_handler=default_exit_handler if required else restart_exit_handler,
-        ) 
+        )
 
-    package, executable,required = "joy_mapper", "joy_mapper_node", True
-    ld.add_process(
-        cmd=[get_executable_path(package_name=package, executable_name=executable),
-            "--publish_topic", "/joy_mapper_node/car_cmd"],
-        name=executable,
-        # die if required, restart otherwise
-        exit_handler=default_exit_handler if required else restart_exit_handler
-    )
-    package, executable,required = "dagu_car", "inverse_kinematics_node", True
-    ld.add_process(
-        cmd=[get_executable_path(package_name=package, executable_name=executable),
-            "--subscribe_topic", "/car_cmd",
-            "--publish_topic", "/wheels_cmd"],
-        name=executable,
-        # die if required, restart otherwise
-        exit_handler=default_exit_handler if required else restart_exit_handler
-    )
-    package, executable, required = "object_classifier", "object_classification_node", True
-    ld.add_process(
-        cmd=[get_executable_path(package_name=package, executable_name=executable),
-            "--subscribe_topic", "/image/compressed",
-            "--publish_topic", "/object_classifier/output"],
-        name=executable,
-        # die if required, restart otherwise
-        exit_handler=default_exit_handler if required else restart_exit_handler
-    )
     return ld
+
--- 80-deep-learning/object_classifier/object_classifier/object_classification.py
+++ 80-deep-learning/object_classifier/object_classifier/object_classification.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python
+import os
+import sys
+import argparse
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 import threading
-import os
-import sys
 import numpy as np
 import cv2
 from mvnc import mvncapi as mvnc
 from duckietown_msgs.msg import ClassifiedObject
 
 
+IMAGE_DIM = (299, 299)
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+GRAPH_FILE = 'graph'
+CATEGORY_FILE = 'categories.txt'
+
+
 class object_classifier(Node):
-    def __init__(self):
+    def __init__(self, args):
         super().__init__('object_classifier')
+
+        self.args = args
+
         self.sub_image = self.create_subscription(CompressedImage, '/image/compressed', self.call_back)
         self.pub_obj = self.create_publisher(ClassifiedObject, '/object_classifier/output')
 
-        self.verbose = True
-        self.min_score_threshold = 0.5
-
-        self.IMAGE_DIM = (299, 299)
-        self.BASE_PATH = os.path.dirname(os.path.realpath(__file__))
-        self.GRAPH_NAME = 'graph'
-        self.CATEGORY_FILE = 'categories.txt'
+        self.verbose = self.args.verbose 
+        self.min_score_threshold = self.args.min_score_threshold
 
         self.prev_category = None
         self.categories = []
-        with open('/home/david/duckietown2/src/duckietown2/80-deep-learning/object_classifier/object_classifier/inception-v4/categories.txt', 'r') as f:
+        with open('{}/inception-v4/{}'.format(BASE_PATH, CATEGORY_FILE), 'r') as f:
             for line in f:
                 cat = line.split('\n')[0]
                 if cat != 'classes':
@@ -43,7 +48,7 @@ class object_classifier(Node):
         device = mvnc.Device(devices[0])
         device.OpenDevice()
         
-        with open('/home/david/duckietown2/src/duckietown2/80-deep-learning/object_classifier/object_classifier/inception-v4/graph', mode='rb') as f:
+        with open('{}/inception-v4/{}'.format(BASE_PATH, GRAPH_FILE), mode='rb') as f:
             graph_in_memory = f.read()
 
         self.graph = device.AllocateGraph(graph_in_memory)
@@ -67,7 +72,7 @@ class object_classifier(Node):
         np_arr = np.fromstring(data.data, np.uint8)
         cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-        cv_image = self.resize_image(cv_image, self.IMAGE_DIM)
+        cv_image = self.resize_image(cv_image, IMAGE_DIM)
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
         cv_image = self.normalize_image(cv_image)
 
@@ -81,14 +86,14 @@ class object_classifier(Node):
             'index': top_index,
         }
 
-        if self.verbose:
+        if self.verbose == 'True' or self.verbose == 'true':
             print(detected_obj)
 
         if detected_obj['score'] > self.min_score_threshold:
             if self.prev_category != detected_obj['label']:
                 self.prev_category = detected_obj['label']
                 output = ClassifiedObject()
-                # output.header.stamp = data.header.stamp
+                output.header.stamp = data.header.stamp
                 output.label = detected_obj['label']
                 output.index = int(detected_obj['index'])
                 output.score = float(detected_obj['score'])
@@ -98,7 +103,7 @@ class object_classifier(Node):
     def resize_image(self, img, to_dim):
         dx, dy, dz = img.shape
         delta = float(abs(dy - dx))
-        if dx > dy:  #crop the x dimension
+        if dx > dy:
             img = img[int(0.5 * delta):dx - int(0.5 * delta), 0:dy]
         else:
             img = img[0:dx, int(0.5 * delta):dy - int(0.5 * delta)]
@@ -114,15 +119,37 @@ class object_classifier(Node):
 
 
 def main(args=None):
+    if args is None:
+        args = sys.argv
+
     rclpy.init(args=args)
 
-    oc = object_classifier()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--verbose",
+        type=str,
+        default="True",
+    )
+    parser.add_argument(
+        "--min_score_threshold",
+        type=float,
+        default=0.5,
+    )
+    parser.add_argument(
+        "--camera_topic",
+        type=str,
+        default="/image/compressed",
+    )
+    parser.add_argument(
+        "--object_topic",
+        type=str,
+        default="/object_classifier/output",
+    )
+
+    args = parser.parse_args()
+    oc = object_classifier(args)
     rclpy.spin(oc)
 
     oc.destroy_node()
     rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
 
